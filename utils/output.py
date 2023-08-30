805fd700d3609a50d352b0927c3f8d85b2b66b5f
Hung Vu <leertet96@gmail.com>
2023-08-30 21:34:02 +0200

    Adjust barplot

--- utils/output.py
+++ utils/output.py
@@ -12,9 +12,15 @@ def write_to_file(results):
 def display_plot(results):
     df = pd.DataFrame(results, columns=["Character", "Count"])
     char_plot = sns.barplot(
-        x=df["Count"],
-        y=df["Character"],
-        orient="h",
+        x=df["Count"], y=df["Character"], orient="h", palette="viridis"
     )
     char_plot.tick_params(left=False)
+
+    # Add count labels to the end of each bar
+    label_offset = 0.4
+    for index, row in df.iterrows():
+        char_plot.text(
+            row["Count"] + label_offset, index, str(row["Count"]), va="center"
+        )
+
     plt.show()
